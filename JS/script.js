8d85413ae9bf8b4142ff64a67a2f324cb1a08724
hookor <115582699+hookor@users.noreply.github.com>
2023-03-27 19:24:36 +0900

    3 files modified

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,37 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function() {
+searchEl.addEventListener('click', function () {
   searchInputEl.focus();
 });
 
-searchInputEl.addEventListener('focus', function() {
+searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
-})
-searchInputEl.addEventListener('blur', function() {
+});
+searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
-})
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener(
+  'scroll',
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // badgeEl.style.display = 'none';
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      // badgeEl.style.display = 'block';
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
